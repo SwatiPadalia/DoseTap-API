8b6fd3f7c8f069cab44cc08f30f0fe8abb3e3ed7
souravlahoti <sourav26721062@gmail.com>
2022-01-12 23:26:51 +0530

    added search status sort filter to all company

--- src/controllers/admin/company/company.controller.js
+++ src/controllers/admin/company/company.controller.js
@@ -72,18 +72,51 @@ export const findById = async (req, res) => {
 
 export const all = async (req, res) => {
     try {
+
+        let searchFilter = null, statusFilter = null;
+
+        const sort = req.query.sort || -1;
+
+        if (req.query.search) {
+            const search = req.query.search;
+
+            searchFilter = {
+                [Op.or]: [
+                    sequelize.where(
+                        sequelize.fn('LOWER', sequelize.col('name')), { [Op.like]: `%${search}%` }
+                    )
+                ]
+            }
+        }
+
+        if (req.query.status) {
+            const status = req.query.status;
+            if (status == 1) {
+                statusFilter = true
+            } else {
+                statusFilter = false
+            }
+        }
+
+
         const page = req.query.page || 1;
         const limit = 10;
+        const sortOrder = sort == -1 ? 'ASC' : 'DESC';
         const companies = await Company.findAndCountAll({
-            order: [['createdAt', 'DESC'], ['id', 'ASC']],
+            where: {
+                [Op.and]: [statusFilter === null ? undefined : { status: statusFilter }, searchFilter === null ? undefined : { searchFilter }]
+            },
+            order: [['id', sortOrder]],
             offset: (page - 1) * limit,
             limit,
         });
-        return successResponse(req, res, { companies: {
-            ...companies,
-            currentPage: parseInt(page),
-            totalPage: Math.ceil(companies.count/limit)
-        } });
+        return successResponse(req, res, {
+            companies: {
+                ...companies,
+                currentPage: parseInt(page),
+                totalPage: Math.ceil(companies.count / limit)
+            }
+        });
     } catch (error) {
         return errorResponse(req, res, error.message);
     }
