5825ce2a76ed9e51e2d33b01e4ff86e5402b3563
souravlahoti <sourav26721062@gmail.com>
2022-04-27 18:33:13 +0530

    tracker fixes

--- src/controllers/user/device-data/data.controller.js
+++ src/controllers/user/device-data/data.controller.js
@@ -9,32 +9,102 @@ export const tracker = async (req, res) => {
         const { date } = req.body;
         // const user = await User.findOne({ where: { id: userId } });
 
-        const adherence = await Adherence.findAll({
-            where: {
-                patient_id: userId,
-                date: {
-                    [Op.eq]: date
-                }
-            }
-        })
-
         let morning = false;
         let afternoon = false;
         let evening = false;
         let night = false;
 
-        adherence.map(d => {
+        let inputDate = new Date(date);
+        let todaysDate = new Date();
 
-            const time = parseInt(d.time.split(':').join('').slice(0, -2))
-            if (time >= 900 && time <= 1100 && d.status == "open")
-                morning = true
-            if (time >= 1200 && time <= 1400 && d.status == "open")
-                afternoon = true
-            if (time >= 1500 && time <= 1800 && d.status == "open")
-                evening = true
-            if (time >= 1900 && time <= 2100 && d.status == "open")
-                night = true
-        })
+        if (inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0)) {
+
+            morning = null;
+            afternoon = null;
+            evening = null;
+            night = null;
+
+            var d = new Date();
+            var curr_hour = d.getHours();
+            var curr_min = d.getMinutes();
+            curr_min = curr_min.toString().length == 1 ? "0" + curr_min : curr_min
+            var curr_time = curr_hour + ":" + curr_min + ":" + "00";
+            console.log(' True ', curr_time)
+
+            const todays_adherence = await Adherence.findAll({
+                where: {
+                    patient_id: userId,
+                    date: {
+                        [Op.eq]: date,
+                    },
+                    time: {
+                        [Op.lte]: curr_time
+                    }
+                }
+            })
+
+            console.log(' adherence ', todays_adherence)
+
+            todays_adherence.map(d => {
+                const time = parseInt(d.time.split(':').join('').slice(0, -2))
+                console.log("🚀 ~ file: data.controller.js ~ line 50 ~ tracker ~ time", time)
+                if (time >= 900 && time <= 1100) {
+                    if (d.status == "open")
+                        morning = true;
+                    else
+                        morning = false;
+                    afternoon = null;
+                    evening = null;
+                    night = null;
+                }
+
+                if (time >= 1200 && time <= 1400) {
+                    if (d.status == "open")
+                        afternoon = true;
+                    else
+                        afternoon = false;
+                    evening = null;
+                    night = null;
+                }
+                if (time >= 1500 && time <= 1800) {
+                    if (d.status == "open")
+                        evening = true;
+                    else
+                        evening = false;
+                    night = null;
+                }
+                if (time >= 1900 && time <= 2100) {
+                    if (d.status == "open")
+                        night = true;
+                    else
+                        night = false;
+                }
+            })
+
+        } else {
+
+            const adherence = await Adherence.findAll({
+                where: {
+                    patient_id: userId,
+                    date: {
+                        [Op.eq]: date
+                    }
+                }
+            })
+
+            adherence.map(d => {
+
+                const time = parseInt(d.time.split(':').join('').slice(0, -2))
+                if (time >= 900 && time <= 1100 && d.status == "open")
+                    morning = true
+                if (time >= 1200 && time <= 1400 && d.status == "open")
+                    afternoon = true
+                if (time >= 1500 && time <= 1800 && d.status == "open")
+                    evening = true
+                if (time >= 1900 && time <= 2100 && d.status == "open")
+                    night = true
+            })
+        }
 
 
         let missedTill = 0;
@@ -49,7 +119,7 @@ export const tracker = async (req, res) => {
             else missedTill += 1
         })
 
-        const tillNow = (openTill / (openTill + missedTill) * 100) || 0
+        const tillNow = (openTill / (openTill + missedTill) * 100).toFixed(2) || 0
 
 
         let new_date = new Date(date);
@@ -72,7 +142,7 @@ export const tracker = async (req, res) => {
             else missedTillMonth += 1
         })
 
-        const tillMonth = (openTillMonth / (openTillMonth + missedTillMonth) * 100) || 0
+        const tillMonth = (openTillMonth / (openTillMonth + missedTillMonth) * 100).toFixed(2) || 0
 
         return successResponse(req, res, { morning, afternoon, evening, night, tillNow, tillMonth });
     } catch (error) {
